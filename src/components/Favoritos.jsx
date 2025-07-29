3b5e299509259beaf5889852fbf99876bb2d38d0
MariaJose <mariajoserucla@gmail.com>
2025-07-29 10:57:39 +0200

    favoritos creado

---
+++ src/components/Favoritos.jsx
@@ -0,0 +1,19 @@
+import ListaRecetas from "./ListaRecetas";
+
+function Favoritos({ favoritos, obtenerDetallesReceta, toggleFavorito}) {
+if (favoritos.lenght === 0) {
+    return <p className="mt-6 text-center text-gray-500">No tienes recetas favoritas.</p>;
+}
+return (
+    <div className="mt-10">
+        <h2 className="text-xl font-bold mb-4">Mis recetas favoritas</h2>
+        <ListaRecetas
+        recetas={favoritos}
+        obtenerDetallesReceta={obtenerDetallesReceta}
+        favoritos={favoritos}
+        toggleFavorito={toggleFavorito}
+        />
+    </div>
+);
+}
+export default Favoritos;
